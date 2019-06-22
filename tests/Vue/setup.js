65740777f142db0a2aa43d0c3daf26624ce64616
tristar <tristar@urban-theory.net>
2019-06-22 15:46:34 +0900

    Eslintのエラー抑止

--- tests/Vue/setup.js
+++ tests/Vue/setup.js
@@ -3,5 +3,4 @@ require("jsdom-global")()
 global.expect = require("expect")
 global.axios = require("axios")
 global.Vue = require("vue")
-global.bus = new Vue()
-ww
+global.bus = new Vue() // eslint-disable-line no-undef
