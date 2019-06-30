7020f5c93a6d993eb9a5eb7ceb6eddc716965aca
tristar <tristar@urban-theory.net>
2019-06-30 23:39:36 +0900

    ダッシュボードにプロジェクト一覧を追加(途中)

---
+++ resources/js/pages/dashboard/project-list.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <a class="command-button">
+            <i class="icon fas fa-plus-circle"></i>新規作成
+        </a>
+        <table class="small-table project-list">
+            <tr>
+                <td><a href="#">プロジェクトA</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">プロジェクトB</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">プロジェクトC</a></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
