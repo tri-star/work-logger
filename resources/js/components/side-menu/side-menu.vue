535f9adb8f2c79d2e430d449d0f198a116d25aab
tristar <tristar@urban-theory.net>
2019-07-14 03:29:36 +0900

    サイドメニューはFOnt Awesomeを利用するように変更

--- resources/js/components/side-menu/side-menu.vue
+++ resources/js/components/side-menu/side-menu.vue
@@ -1,7 +1,7 @@
 <template>
     <aside class="side">
         <div class="menu-content">
-            <div class="item icon-toggle" @click="$emit('toggle')"></div>
+            <div class="item fas fa-bars" @click="$emit('toggle')"></div>
             <router-link
                 v-for="(menuItem, index) in menuList"
                 tag="div"
@@ -13,14 +13,14 @@
             </router-link>
         </div>
         <div class="menu-content-small">
-            <div class="item icon-toggle" @click="$emit('toggle')"></div>
+            <div class="item fas fa-bars" @click="$emit('toggle')"></div>
             <router-link
                 v-for="(menuItem, index) in menuList"
                 tag="div"
                 :to="menuItem.to"
                 :class="`item ${menuItem.icon}`"
                 :key="index"
-            >
+                >&nbsp;
             </router-link>
         </div>
     </aside>
@@ -29,17 +29,17 @@
 <script>
 const menues = {
     default: [
-        { icon: "icon-stats", label: "統計情報", to: "" },
-        { icon: "icon-config", label: "設定", to: "" }
+        { icon: "fas fa-chart-pie", label: "統計情報", to: "" },
+        { icon: "fas fa-cog", label: "設定", to: "" }
     ],
     project: [
-        { icon: "icon-dashboard", label: "ダッシュボード", to: "/" },
+        { icon: "fas fa-chart-line", label: "ダッシュボード", to: "/" },
         {
-            icon: "icon-tasks",
+            icon: "fas fa-tasks",
             label: "タスク一覧",
             to: "/project/:id/tasks"
         },
-        { icon: "icon-config", label: "設定", to: "" }
+        { icon: "fas fa-cog", label: "設定", to: "" }
     ]
 }
 
@@ -89,11 +89,13 @@ aside {
     background-color: $side-menu-background-color;
 
     .item {
-        margin: 10px auto;
+        margin: 3px auto;
         cursor: pointer;
-        padding: 5px 0 5px 35px;
+        padding: 5px 0 5px 10px;
+        width: 90%;
         height: 30px;
         color: #fff;
+        font-size: 1.2rem;
         transition: padding, background-color 0.3s;
     }
 
@@ -101,29 +103,8 @@ aside {
         background-color: lighten($side-menu-background-color, 5%);
         background-position-y: 3px;
         box-shadow: 2px 2px 0 rgba(255, 255, 255, 0.4);
-        padding: 3px 0 5px 35px;
+        padding: 3px 0 5px 10px;
         transition: padding, background-color 0.3s;
-    }
-
-    .icon-toggle {
-        display: block;
-        background: url("/images/menu-toggle.png") no-repeat 5px 5px;
-    }
-    .icon-dashboard {
-        display: block;
-        background: url("/images/menu-dashboard.png") no-repeat 5px 5px;
-    }
-    .icon-stats {
-        display: block;
-        background: url("/images/menu-stats.png") no-repeat 5px 5px;
-    }
-    .icon-tasks {
-        display: block;
-        background: url("/images/menu-tasks.png") no-repeat 5px 5px;
-    }
-    .icon-config {
-        display: block;
-        background: url("/images/menu-config.png") no-repeat 5px 5px;
     }
 }
 </style>
