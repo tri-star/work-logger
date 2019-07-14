35cd3210790932f903c911a141eda2809b75f041
tristar <tristar@urban-theory.net>
2019-07-15 02:14:45 +0900

    フォームをコンテナコンポーネントと表示コンポーネントに分割

--- resources/js/pages/tasks/task-form-container.vue
+++ resources/js/pages/tasks/task-form-container.vue
@@ -1,71 +1,12 @@
 <template>
     <WlModal :showModal="showModal">
         <template v-slot:body>
-            <h1 class="heading-1">{{ formTitle }}</h1>
-
-            <div class="form">
-                <div class="row">
-                    <div class="col-header">タスク名</div>
-                    <div class="col">
-                        <input
-                            type="text"
-                            v-model="task.title"
-                            class="text-box task-name"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-header">開始予定日</div>
-                    <div class="col">
-                        <input
-                            type="text"
-                            v-model="task.start_date"
-                            class="text-box"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-header">完了予定日</div>
-                    <div class="col">
-                        <input
-                            type="text"
-                            v-model="task.end_date"
-                            class="text-box"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-header">予定工数</div>
-                    <div class="col">
-                        <input
-                            type="number"
-                            v-model="task.estimate_minutes"
-                            class="text-box estimate-time"
-                        />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-header">ステータス</div>
-                    <div class="col">
-                        <select v-model="task.status">
-                            <option
-                                v-for="(statusName, value) of statuses"
-                                :value="value"
-                                :key="value"
-                                >{{ statusName }}</option
-                            >
-                        </select>
-                    </div>
-                </div>
-                <div class="col-center button-area">
-                    <button class="button" @click="save">
-                        {{ submitButtonTitle }}
-                    </button>
-                    <button class="button" @click="showModal = false">
-                        キャンセル
-                    </button>
-                </div>
-            </div>
+            <TaskForm
+                :id="id"
+                :task="task"
+                @save="handleSave"
+                @close="handleClose"
+            />
         </template>
     </WlModal>
 </template>
@@ -73,6 +14,7 @@
 <script>
 import WlModal from "../../components/wl-modal"
 import Task from "../../domain/task"
+import TaskForm from "./task-form"
 import AdapterFactory from "../../adapters/adapter-factory"
 
 export default {
@@ -84,13 +26,12 @@ export default {
     },
 
     components: {
-        WlModal
+        WlModal,
+        TaskForm
     },
     data() {
         return {
             id: 0,
-            formTitle: "",
-            submitButtonTitle: "",
             showModal: false,
             statuses: Task.getStatusNames(),
             task: {}
@@ -105,8 +46,6 @@ export default {
         open(id) {
             this.id = id
             this.showModal = true
-            this.formTitle = "タスク登録"
-            this.submitButtonTitle = "登録"
             if (this.id === 0) {
                 this.task = {
                     title: "",
@@ -116,8 +55,6 @@ export default {
                     status: 0
                 }
             } else {
-                this.formTitle = "タスク更新"
-                this.submitButtonTitle = "更新"
                 this.loadTask(this.id)
                     .then(task => {
                         this.task = task
@@ -128,12 +65,15 @@ export default {
                     })
             }
         },
-        save() {
+        handleSave() {
             if (this.id === 0) {
                 this.addTask()
             } else {
                 this.updateTask()
             }
+        },
+        handleClose() {
+            this.showModal = false
         },
         addTask() {
             const adapter = AdapterFactory.get("TaskAdapter")
@@ -167,43 +107,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.task-name {
-    width: 300px;
-}
-.estimate-time {
-    width: 60px;
-}
-
-.form {
-    width: 600px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 10px;
-
-    .row {
-        display: flex;
-        justify-content: flex-start;
-        align-content: center;
-        padding: 10px;
-
-        .col-header {
-            width: 120px;
-        }
-        .col {
-            width: 400px;
-        }
-        .col-center {
-            width: 100px;
-        }
-    }
-    .button {
-        margin-right: 20px;
-    }
-
-    .button-area {
-        margin-top: 10px;
-    }
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ resources/js/pages/tasks/task-form.vue
@@ -0,0 +1,147 @@
+<template>
+    <div>
+        <h1 class="heading-1">{{ formTitle }}</h1>
+
+        <div class="form">
+            <div class="row">
+                <div class="col-header">タスク名</div>
+                <div class="col">
+                    <input
+                        type="text"
+                        v-model="task.title"
+                        class="text-box task-name"
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-header">開始予定日</div>
+                <div class="col">
+                    <input
+                        type="text"
+                        v-model="task.start_date"
+                        class="text-box"
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-header">完了予定日</div>
+                <div class="col">
+                    <input
+                        type="text"
+                        v-model="task.end_date"
+                        class="text-box"
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-header">予定工数</div>
+                <div class="col">
+                    <input
+                        type="number"
+                        v-model="task.estimate_minutes"
+                        class="text-box estimate-time"
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-header">ステータス</div>
+                <div class="col">
+                    <select v-model="task.status">
+                        <option
+                            v-for="(statusName, value) of statuses"
+                            :value="value"
+                            :key="value"
+                            >{{ statusName }}</option
+                        >
+                    </select>
+                </div>
+            </div>
+            <div class="col-center button-area">
+                <button class="button" @click="$emit('save')">
+                    {{ buttonTitle }}
+                </button>
+                <button class="button" @click="$emit('close')">
+                    キャンセル
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Task from "../../domain/task"
+
+export default {
+    props: {
+        id: {
+            type: Number,
+            required: true
+        },
+        task: {
+            type: Object,
+            required: true
+        }
+    },
+
+    components: {},
+    data() {
+        return {
+            statuses: Task.getStatusNames()
+        }
+    },
+
+    computed: {
+        formTitle() {
+            return this.id === 0 ? "タスク登録" : "タスク更新"
+        },
+        buttonTitle() {
+            return this.id === 0 ? "登録" : "更新"
+        }
+    },
+
+    methods: {},
+
+    mounted() {}
+}
+</script>
+
+<style lang="scss" scoped>
+.task-name {
+    width: 300px;
+}
+.estimate-time {
+    width: 60px;
+}
+
+.form {
+    width: 600px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+
+    .row {
+        display: flex;
+        justify-content: flex-start;
+        align-content: center;
+        padding: 10px;
+
+        .col-header {
+            width: 120px;
+        }
+        .col {
+            width: 400px;
+        }
+        .col-center {
+            width: 100px;
+        }
+    }
+    .button {
+        margin-right: 20px;
+    }
+
+    .button-area {
+        margin-top: 10px;
+    }
+}
+</style>
