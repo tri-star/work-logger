7599750da7e50cae9df58a01fe19be6021740b05
tristar <tristar@urban-theory.net>
2019-07-15 02:14:52 +0900

    veevalidateを追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Vuex from "vuex"
 import VueRouter from "vue-router"
+import VeeValidate from "vee-validate"
 import initStore from "./store"
 import routes from "./routes"
 import DefaultLayout from "./pages/default-layout"
@@ -10,6 +11,7 @@ require("es6-promise/auto")
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
+Vue.use(VeeValidate)
 window.Vue = Vue
 
 const router = new VueRouter({ mode: "history", routes })
