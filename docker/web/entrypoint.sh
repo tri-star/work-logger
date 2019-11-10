2872ac1363cb1889e44ee40d6bd1727e47830d81
tristar <tristar@urban-theory.net>
2019-11-10 17:10:31 +0000

    本番環境用コンテナのDockerfileを作成 以下が未完成 * nginxとphp-fpmはコンテナを分けて非rootで稼働させる * DBコンテナの設定(my.cnf) * secretの対応 * psやiproute2関連のコマンドの標準インストール

---
+++ docker/web/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p /var/log/nginx
+
+
+APP_DIR=/work-logger
+chmod -R go+w $APP_DIR/bootstrap/cache
+mkdir -p $APP_DIR/storage/app/public
+mkdir -p $APP_DIR/storage/framework/cache
+mkdir -p $APP_DIR/storage/framework/sessions
+mkdir -p $APP_DIR/storage/framework/views
+mkdir -p $APP_DIR/storage/logs
+chmod -R go+w $APP_DIR/storage
+
+/usr/bin/supervisord -n
+
+exec "$@"
