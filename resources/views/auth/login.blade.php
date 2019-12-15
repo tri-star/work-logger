5f5e0907b0303b917c3c52b1f852a4ba347c706b
tristar <tristar@urban-theory.net>
2019-12-15 09:41:36 +0000

    ログイン画面のデザイン見直し

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,48 +6,48 @@
 
 @section('content')
 
-        <div class="logo"></div>
-            <form action="{{ route('login')  }}" method="POST" class="frame">
-                @csrf
+<h1>WORK LOGGER</h1>
 
-                <div class="row">
-                    <div class="col-header">
-                        <label>LOGIN ID: </label>
-                    </div>
-                    <div class="col">
-                        <input type="text" class="text-box" name="email" id="email">
-                        @if ($errors->has('email'))
-                            <p class="invalid-feedback" role="alert">
-                                <span>{{ $errors->first('email') }}</span>
-                            </p>
-                        @endif
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-header">
-                        <label>PASSWORD: </label>
-                    </div>
-                    <div class="col">
-                        <input type="password" class="text-box" name="password" id="password">
-                        @if ($errors->has('password'))
-                        <p class="invalid-feedback" role="alert">
-                            <span>{{ $errors->first('password') }}</span>
-                        </p>
-                        @endif
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="remember"><input type="checkbox" name="remember" id="remember" value="1"/>ID、パスワードを保存</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <button class="button">LOGIN</button>
-                    </div>
-                </div>
+<form action="{{ route('login')  }}" method="POST">
+    @csrf
 
-            </form>
+    <div class="row">
+        <div class="col-header">
+            <label>Login ID: </label>
         </div>
+        <div class="col">
+            <input type="text" class="text-box" name="email" id="email">
+            @if ($errors->has('email'))
+                <p class="invalid-feedback" role="alert">
+                    <span>{{ $errors->first('email') }}</span>
+                </p>
+            @endif
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-header">
+            <label>Password: </label>
+        </div>
+        <div class="col">
+            <input type="password" class="text-box" name="password" id="password">
+            @if ($errors->has('password'))
+            <p class="invalid-feedback" role="alert">
+                <span>{{ $errors->first('password') }}</span>
+            </p>
+            @endif
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <label for="remember"><input type="checkbox" name="remember" id="remember" value="1"/>ログイン状態を保持</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-login-button">
+            <button class="button-lg">Login</button>
+        </div>
+    </div>
+
+</form>
 
 @endsection
