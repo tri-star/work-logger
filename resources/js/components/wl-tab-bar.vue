3177492d1bbd7f0213187e66a8d83286f27196d2
tristar <tristar@urban-theory.net>
2020-02-25 15:45:06 +0000

    タブ用コンポーネントの追加

---
+++ resources/js/components/wl-tab-bar.vue
@@ -0,0 +1,65 @@
+<template>
+  <ul class="tab-bar clear-fix">
+    <li v-for="item of items" :key="item.tabKey">
+      <WlTabItem :label="item.label" :tab-key="item.tabKey" :is-active="isActive(item.tabKey)" @selected="handleSelected" />
+    </li>
+  </ul>
+</template>
+
+<script>
+
+import WlTabItem from './wl-tab-item'
+
+export default {
+
+  components: {
+    WlTabItem
+  },
+
+  props: {
+    items: {
+      type: Object,
+      required: true
+    },
+    initialSelected: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data () {
+    return {
+      current: ''
+    }
+  },
+
+  mounted () {
+    this.current = this.initialSelected
+  },
+  methods: {
+    isActive (key) {
+      return this.current === key
+    },
+    handleSelected (selectedKey) {
+      this.current = selectedKey
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import "../../sass/imports";
+
+.tab-bar {
+  list-style: none;
+  padding: 0;
+  border-bottom: 2px solid rgba(0,0,0,0.3);
+  margin-bottom: 10px;
+
+  li {
+    float: left;
+  }
+}
+
+</style>
